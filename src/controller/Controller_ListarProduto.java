2771b9492771625a75f8055d90bc349a3a80b59c
Manuel Maria dos Santos Neto <manelsantis04@gmail.com>
2021-05-03 23:09:38 -0300

    Médodo de pesquisa por nome e código do produto completo

--- src/controller/Controller_ListarProduto.java
+++ src/controller/Controller_ListarProduto.java
@@ -1,8 +1,11 @@
 package controller;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
+import Exception.ExceptionCampoInvalido;
 import Model.BO.ProdutoBO;
 import Model.VO.ProdutoVO;
 import javafx.collections.FXCollections;
@@ -11,9 +14,11 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.paint.Color;
 import view.Telas;
@@ -42,9 +47,16 @@ public class Controller_ListarProduto implements Initializable {
 	private Button editar;
 	@FXML
 	private Button voltar;
-	
+	@FXML
+	private Button pesquisar;
+
+	@FXML
+	private TextField pesquisa;
+
 	@FXML
 	private Label mensagem;
+	@FXML
+	private ComboBox<String> escolha;
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
@@ -56,6 +68,12 @@ public class Controller_ListarProduto implements Initializable {
 		peso.setCellValueFactory(new PropertyValueFactory<ProdutoVO, Double>("peso"));
 		id.setCellValueFactory(new PropertyValueFactory<ProdutoVO, Long>("id"));
 		lista.setItems(produtos);
+
+		List<String> categorias = new ArrayList<String>();
+		categorias.add("Nome");
+		categorias.add("Código");
+		cb = FXCollections.observableArrayList(categorias);
+		escolha.setItems(cb);
 	}
 
 	@FXML
@@ -90,8 +108,55 @@ public class Controller_ListarProduto implements Initializable {
 	}
 
 	@FXML
+	public void pesquisar(ActionEvent event) throws ExceptionCampoInvalido {
+		if ((pesquisa.getText() != null) && (!pesquisa.getText().isEmpty())) {
+			if (escolha.getSelectionModel().getSelectedItem() != null) {
+
+				ProdutoBO pesquisando = new ProdutoBO();
+				ProdutoVO prod = new ProdutoVO();
+				if (escolha.getSelectionModel().getSelectedItem().equals("Nome")) {
+					// para caso escolha pesquisar pelo nome
+					mensagem.setVisible(true); // caso a mensagem não esteja ativa
+					mensagem.setTextFill(Color.web("green"));
+					mensagem.setText("Pesquisa executada");
+					prod.setNome(pesquisa.getText());
+					ObservableList<ProdutoVO> produtos = FXCollections.observableArrayList(pesquisando.findByName(prod));
+					nome.setCellValueFactory(new PropertyValueFactory<ProdutoVO, String>("nome"));
+					quantidade.setCellValueFactory(new PropertyValueFactory<ProdutoVO, Integer>("quantidade"));
+					preco.setCellValueFactory(new PropertyValueFactory<ProdutoVO, Double>("preco"));
+					peso.setCellValueFactory(new PropertyValueFactory<ProdutoVO, Double>("peso"));
+					id.setCellValueFactory(new PropertyValueFactory<ProdutoVO, Long>("id"));
+					lista.setItems(produtos);
+				}
+				if (escolha.getSelectionModel().getSelectedItem().equals("Código")) {
+					// para caso escolha pesquisar pelo nome
+					mensagem.setVisible(true); // caso a mensagem não esteja ativa
+					mensagem.setTextFill(Color.web("green"));
+					mensagem.setText("Pesquisa executada");
+					prod.setNome(pesquisa.getText());
+					ObservableList<ProdutoVO> produtos = FXCollections.observableArrayList(pesquisando.findByCode(prod));
+					nome.setCellValueFactory(new PropertyValueFactory<ProdutoVO, String>("nome"));
+					quantidade.setCellValueFactory(new PropertyValueFactory<ProdutoVO, Integer>("quantidade"));
+					preco.setCellValueFactory(new PropertyValueFactory<ProdutoVO, Double>("preco"));
+					peso.setCellValueFactory(new PropertyValueFactory<ProdutoVO, Double>("peso"));
+					id.setCellValueFactory(new PropertyValueFactory<ProdutoVO, Long>("id"));
+					lista.setItems(produtos);
+				}
+			} else {
+				mensagem.setTextFill(Color.web("red"));
+				mensagem.setText("Selecione um tipo de pesquisa");
+				mensagem.setVisible(true);
+			}
+		} else {
+			mensagem.setTextFill(Color.web("red"));
+			mensagem.setText("Digite algo para pesquisar");
+			mensagem.setVisible(true);
+		}
+	}
+
+	@FXML
 	public void voltar(ActionEvent event) {
-		
+
 	}
 
 }
